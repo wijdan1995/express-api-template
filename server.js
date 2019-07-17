df0c8fb20af8310cda7c54ccc80bda776e7870c8
MicFin <mrfinneran@gmail.com>
2019-07-17 20:19:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const app = express()
 
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || reactPort }))
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}`}))
 
 // define port for API to run on
 const port = process.env.PORT || expressPort
